d4888718e8a8379a163373f8b271ec431e18b553
Emanuele Manfredini <emanuele.manfredini.1992@gmail.com>
2024-06-08 05:11:18 +0200

    initial commit

---
+++ django_dbviews/settings.py
@@ -0,0 +1,24 @@
+from config.database_config import db_config
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+
+]
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = db_config
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
